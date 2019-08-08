e148e4162e6b91ec9db54a5964ede42e307600e8
Keheliya Gallaba <keheliya.gallaba@gmail.com>
2019-08-08 06:41:33 -0400

    Create yourbased.sh

---
+++ yourbased.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -ex
+export DEBIAN_FRONTEND=noninteractive
+export NOKOGIRI_USE_SYSTEM_LIBRARIES=true
+apt-get update && apt-get install -y tzdata
+gem install bundler -v 2.0.1
+# install
+bundle install --jobs=3 --retry=3
+# before_script
+bundle exec bin/setup
+# script
+rake
