9979da5bfb70871ba7ff08491d546bc8ffb8059c
Keheliya Gallaba <keheliya.gallaba@gmail.com>
2019-08-08 06:59:01 -0400

    Update yourbased.sh

--- yourbased.sh
+++ yourbased.sh
@@ -4,6 +4,7 @@ export DEBIAN_FRONTEND=noninteractive
 export NOKOGIRI_USE_SYSTEM_LIBRARIES=true
 apt-get update && apt-get install -y tzdata libmysqlclient-dev
 gem install bundler -v 2.0.1
+gem install pkg-config -v "~> 1.1"
 # install
 bundle install --jobs=3 --retry=3
 # before_script
